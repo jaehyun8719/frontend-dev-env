c2dc790797656c1def3eef2006f74c8f45d0a6d8
jaehyun8719 <donald@greenlamp.kr>
2020-06-05 17:01:11 +0900

    file-loader & url-loader

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-import './style.css'
+import "./style.css"
+import small from './small.png';
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.innerHTML = `
+        <img src="${small}" />
+    `
+})
--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,21 @@ module.exports = {
         path: path.resolve("./dist"),
     },
     module: {
-        rules: [{
-            test: /\.css$/, // .css 확장자로 끝나는 모든 파일
-            use: ["style-loader", "css-loader"], // style-loader를 적용
-        }],
+        rules: [
+            {
+                test: /\.css$/, // .css 확장자로 끝나는 모든 파일
+                use: ["style-loader", "css-loader"], // style-loader를 적용
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: {
+                    loader: "url-loader", // url 로더를 설정한다
+                    options: {
+                        name: "[name].[ext]?[hash]", // file-loader와 동일
+                        limit: 100000 // 100kb 미만 파일만 data url로 처리
+                    }
+                }
+            }
+        ],
     }
 }
