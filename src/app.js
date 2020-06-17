0164ab46ce9ba2e8ca038ad0731165898a9f9318
jaehyun8719 <donald@greenlamp.kr>
2020-06-17 16:43:02 +0900

    polyfile 설정

--- src/app.js
+++ src/app.js
@@ -7,13 +7,27 @@ document.addEventListener('DOMContentLoaded', () => {
     `
 })
 
-console.log(VERSION) // 'v.1.2.3'
-console.log(PRODUCTION) // true
-console.log(MAX_COUNT) // 999
-console.log(api.domain) // 'http://dev.api.domain.com'
+// console.log(VERSION) // 'v.1.2.3'
+// console.log(PRODUCTION) // true
+// console.log(MAX_COUNT) // 999
+// console.log(api.domain) // 'http://dev.api.domain.com'
 
 // babel
 const name = "jaehyun";
 console.log(name);
 
-new Promise();
+// core-js
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('foo');
+    }, 300);
+});
+
+promise1.then((value) => {
+    console.log(value);
+    // expected output: "foo"
+});
+
+console.log(promise1);
+// expected output: [object Promise]
+
